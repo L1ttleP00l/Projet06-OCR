67dbabb31a955069716ea0eed500ac0fc30cbd94
L1ttleP00l <bastien.barillot@gmail.com>
2023-08-22 20:03:04 +0200

    index.html => Anchor + Aria-Label for logo / style.css => Modification "hover" + Class / photographer.js => Update elements

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -4,22 +4,29 @@ function photographerTemplate(data) {
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        const a = document.createElement('a')
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        const h3 = document.createElement('h3');
-        const h4 = document.createElement('h4')
-        const h5 = document.createElement('h5')
+        img.setAttribute("alt", "Portrait de " + name)
+        img.setAttribute("aria-label", "Sélection du photographe " + name)
+        const h2 = document.createElement('h2');
+        const pcity = document.createElement('p');
+        pcity.setAttribute("class", "city")
+        const ptagline = document.createElement('p')
+        ptagline.setAttribute("class", "tagline")
+        const pprice = document.createElement('p')
+        pprice.setAttribute("class", "price")
         h2.textContent = name;
-        h3.textContent = city + ", " + country
-        h4.textContent = tagline
-        h5.textContent = price + "€/jour"
-        article.appendChild(img);
-        article.appendChild(h2);
-        article.appendChild(h3);
-        article.appendChild(h4);
-        article.appendChild(h5);
+        pcity.textContent = city + ", " + country
+        ptagline.textContent = tagline
+        pprice.textContent = price + "€/jour"
+        article.appendChild(a);
+        a.appendChild(img);
+        a.appendChild(h2);
+        article.appendChild(pcity);
+        article.appendChild(ptagline);
+        article.appendChild(pprice);
         return (article);
     }
     return { name, picture, getUserCardDOM }
