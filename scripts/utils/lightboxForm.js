08eb48b428f335b00e58e38cff42efd4826039fe
L1ttleP00l <bastien.barillot@gmail.com>
2023-10-11 17:12:58 +0200

    Modification of the lightbox with the addition of previous/next buttons and the close cross + Addition of previous/next keyboard navigation and the "Esc" key to exit the lightbox.

--- scripts/utils/lightboxForm.js
+++ scripts/utils/lightboxForm.js
@@ -1,3 +1,6 @@
+let currentMediaList = null;
+let currentIndex = null;
+
 function openLightbox(mediaSrc, mediaList, index) {
     const media = mediaList[index]
     const lightboxOverlay = document.querySelector('.lightbox-overlay');
@@ -5,9 +8,6 @@ function openLightbox(mediaSrc, mediaList, index) {
     const lightboxMediaContainer = document.getElementById('lightbox-media-container');
     const lightboxTitle = document.getElementById('lightbox-title');
 
-    console.log(mediaList[index])
-
-    // Create a new media element (either image or video)
     const mediaElement = mediaSrc.endsWith('.mp4')
         ? createVideoElement(mediaSrc)
         : createImageElement(mediaSrc);
@@ -16,19 +16,23 @@ function openLightbox(mediaSrc, mediaList, index) {
     lightboxMediaContainer.appendChild(mediaElement);
     lightboxTitle.textContent = media.title;
 
-    // Display lightbox
     lightboxOverlay.style.display = 'block';
     lightboxContent.style.display = 'block';
 
-    // Disable background scrolling
     document.body.style.overflow = 'hidden';
 
-    // Center lightbox horizontally
     lightboxContent.style.left = '50%';
 
-    // Center lightbox vertically in relation to current page position
     const currentScroll = window.scrollY || document.documentElement.scrollTop;
     lightboxContent.style.top = `${currentScroll + (window.innerHeight / 2)}px`;
+
+    currentMediaList = mediaList;
+    currentIndex = index;
+    
+    document.getElementById('lightbox-arrow-left').addEventListener('click', () => previousElement(currentMediaList, currentIndex));
+    document.getElementById('lightbox-arrow-right').addEventListener('click', () => nextElement(currentMediaList, currentIndex));
+
+    document.addEventListener("keydown", handleKeydown);
 }
 
 function createImageElement(imageSrc) {
@@ -52,32 +56,52 @@ function closeLightbox() {
     const lightboxOverlay = document.querySelector('.lightbox-overlay');
     const lightboxContent = document.querySelector('.lightbox-content');
 
-    // Hide lightbox
     lightboxOverlay.style.display = 'none';
     lightboxContent.style.display = 'none';
 
-    // Re-enable background scrolling
     document.body.style.overflow = 'auto';
+
+    document.getElementById('lightbox-arrow-left').removeEventListener('click', () => previousElement(currentMediaList, currentIndex));
+    document.getElementById('lightbox-arrow-right').removeEventListener('click', () => nextElement(currentMediaList, currentIndex));
+
+    document.removeEventListener("keydown", handleKeydown);
+}
+
+function handleKeydown(event) {
+    switch(event.key) {
+        case "ArrowLeft":
+            previousElement(currentMediaList, currentIndex);
+            break;
+        case "ArrowRight":
+            nextElement(currentMediaList, currentIndex);
+            break;
+        case "Escape":
+            closeLightbox();
+            break;
+    }
+}
+
+function changeMedia(mediaList, index) {
+    const media = mediaList[index];
+    const mediaSrc = media.video ? `assets/images/${media.photographerId}/${media.video}` : `assets/images/${media.photographerId}/${media.image}`;
+    const mediaElement = media.video ? createVideoElement(mediaSrc) : createImageElement(mediaSrc);
+
+    const lightboxMediaContainer = document.getElementById('lightbox-media-container');
+    const lightboxTitle = document.getElementById('lightbox-title');
+    
+    lightboxMediaContainer.innerHTML = '';
+    lightboxMediaContainer.appendChild(mediaElement);
+    lightboxTitle.textContent = media.title;
 }
 
 function previousElement(mediaList, index) {
-    let newIndex = index
-    if (index===0) {
-        newIndex = mediaList.length-1
-    } else {
-        newIndex = index-1
-    }
-
-    const newMedia = mediaList[newIndex]
+    let newIndex = index === 0 ? mediaList.length - 1 : index - 1;
+    changeMedia(mediaList, newIndex);
+    currentIndex = newIndex; // Update the global index to the new value
 }
 
 function nextElement(mediaList, index) {
-    let newIndex = index
-    if (index===mediaList.length-1) {
-        newIndex = 0
-    } else {
-        newIndex = index+1
-    }
-
-    const newMedia = mediaList[newIndex]
+    let newIndex = index === mediaList.length - 1 ? 0 : index + 1;
+    changeMedia(mediaList, newIndex);
+    currentIndex = newIndex; // Update the global index to the new value
 }
